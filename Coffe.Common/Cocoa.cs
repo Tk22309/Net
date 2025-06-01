be03c3e38afdde482ee7f6b92d9fe67ff8431cc3
Tk22309 <129153902+Tk22309@users.noreply.github.com>
2025-06-02 01:40:02 +0300

    Add files via upload

---
+++ Coffe.Common/Cocoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffe.Common
+{
+    public class Cocoa : Drink
+    {
+        public static Cocoa CreateNew() => new Cocoa();
+
+        public int Milk { get; set; }
+        public int CocoaPowder { get; set; }
+
+        public static string Type = "Гарячий напій";
+
+        static Cocoa()
+        {
+            Console.WriteLine("Клас Cocoa ініціалізовано");
+        }
+
+        public Cocoa() : base("Cocoa", 220)
+        {
+            Milk = 200;
+            CocoaPowder = 20;
+        }
+    }
+}
